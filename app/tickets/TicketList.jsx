ed638d5ad307dc95f47e59638730f44c1fe00055
argafrl <arga_muhammad@yahoo.co.id>
2023-08-30 23:55:43 +0700

    Add component TicketList

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,25 @@
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets");
+  return res.json();
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map((ticket) => {
+        <div key={ticket.id} className="my-5 card">
+          <h3>{tickets.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+          {tickets.length === 0 && (
+            <p className="text-center">There are no open tickets</p>
+          )}
+        </div>;
+      })}
+    </>
+  );
+}
